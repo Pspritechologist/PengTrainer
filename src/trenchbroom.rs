7f97da1f0175dea8be63a57be126b2a6d349d4fc
Pspritechologist <naaronn@gmail.com>
2026-03-22 14:18:10 -0400

    Cubes and stuff

--- src/trenchbroom.rs
+++ src/trenchbroom.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy_trenchbroom::{anyhow, class::{QuakeClassSpawnView, builtin::FuncGeneric}, config::MapFileFormat, prelude::*};
+use bevy_trenchbroom::{class::builtin::FuncGeneric, config::MapFileFormat, prelude::*};
 use bevy_trenchbroom_avian::AvianPhysicsBackend;
 use avian3d::prelude::{Collider, LinearVelocity, RigidBody};
 use crate::debug::{self, ColorSource};
@@ -17,7 +17,9 @@ impl bevy::prelude::Plugin for Plugin {
 			))
 		.register_type::<FlickeringLight>()
 		.add_systems(PostUpdate, Ball::handle_spawn)
+		.add_systems(PostUpdate, Cube::handle_spawn)
 		.add_systems(FixedUpdate, FlickeringLight::update)
+		.add_systems(PostUpdate, PrototypeMaterial::on_added)
 		;
 	}
 }
@@ -61,63 +63,107 @@ impl FlickeringLight {
 	}
 }
 
-#[base_class(hooks(SceneHooks::new().push(Self::hook)))]
-#[derive(Debug, Default, Component)]
+#[base_class]
+#[derive(Debug, Default, Component, Reflect)]
+#[reflect(Debug, Default, Component)]
 struct PrototypeMaterial {
 	/// The identifier of this brush, such as 'wall' or 'floor'.\
 	/// This can be anything and will be used to generate the brush's colour.
 	name: Option<String>,
 }
 impl PrototypeMaterial {
-	fn hook(view: &mut QuakeClassSpawnView) -> anyhow::Result<()> {
-		let mut entity = view.world.entity_mut(view.entity);
-
-		let name = entity.get::<PrototypeMaterial>().unwrap().name.as_ref();
-
-		let color = match name {
-			Some(name) => name.as_str().color(),
-			None => {
-				entity.get::<Transform>().map_or(Vec3::ZERO, |xform| xform.translation).color()
-			},
-		};
-
-		entity.insert(debug::PrototypeMaterial::new(color));
-
-		Ok(())
+	fn on_added(mut commands: Commands, query: Populated<(Entity, &PrototypeMaterial, Option<&Transform>), Changed<PrototypeMaterial>>) {
+		for (ent, prototype_mat, xform) in query {
+			let color = match prototype_mat.name.as_ref() {
+				Some(name) => name.as_str().color(),
+				None => {
+					xform.map_or(Vec3::ZERO, |xform| xform.translation).color()
+				},
+			};
+	
+			commands.entity(ent).insert(debug::PrototypeMaterial::new(color));
+		}
 	}
 }
 
 #[solid_class(base(FuncGeneric, PrototypeMaterial))]
 #[derive(Debug, Default, Component)]
+#[reflect(Debug, Default, Component)]
 /// A prototype brush, to be assigned a random colour based one
 /// the name if one is provided, or its position otherwise.
 struct PrototypeBrush;
 
-#[point_class(base(Transform))]
-#[derive(Default, Component)]
+#[point_class(
+	base(Transform, PrototypeMaterial),
+	size(-15 -15 -15, 15 15 15),
+	group("debug"),
+)]
+#[derive(Debug, Default, Component)]
+#[reflect(Debug, Default, Component)]
 /// A parkin ball
 pub struct Ball {
 	/// Nyooom.
 	velocity: Vec3,
 }
 
+#[derive(Debug, Resource, Reflect)]
+#[reflect(Debug, Resource)]
+struct BallMesh(Handle<Mesh>);
+
 impl Ball {
 	fn handle_spawn(
 		mut commands: Commands,
-		entities: Query<(Entity, &Ball, Option<&Transform>), Added<Ball>>,
+		entities: Populated<(Entity, &Ball), Added<Ball>>,
+		mesh: Option<Res<BallMesh>>,
 		mut meshes: ResMut<Assets<Mesh>>,
 	) {
-		let mesh = meshes.add(Mesh::from(Sphere::new(0.5)));
+		let mesh = mesh.map_or_else(
+			|| meshes.add(Mesh::from(Sphere::new(0.5))),
+			|mesh| mesh.0.clone(),
+		);
 
-		for (entity, ball, xform) in entities {
-			let pos = xform.map_or(Vec3::ZERO, |xform| xform.translation);
-
+		for (entity, ball) in entities {
 			commands.entity(entity).insert((
-				LinearVelocity(ball.velocity),
 				RigidBody::Dynamic,
 				Collider::sphere(0.5),
+				LinearVelocity(ball.velocity),
 				Mesh3d(mesh.clone()),
-				debug::PrototypeMaterial::new(pos),
+			));
+		}
+	}
+}
+
+#[point_class(
+	base(Transform, PrototypeMaterial),
+	size(-15 -15 -15, 15 15 15),
+	group("debug"),
+)]
+#[derive(Debug, Default, Component)]
+#[reflect(Debug, Default, Component)]
+/// Pushable cube
+pub struct Cube;
+
+#[derive(Debug, Resource, Reflect)]
+#[reflect(Debug, Resource)]
+struct CubeMesh(Handle<Mesh>);
+
+impl Cube {
+	fn handle_spawn(
+		mut commands: Commands,
+		entities: Populated<Entity, Added<Cube>>,
+		mesh: Option<Res<CubeMesh>>,
+		mut meshes: ResMut<Assets<Mesh>>,
+	) {
+		let mesh = mesh.map_or_else(
+			|| meshes.add(Mesh::from(Cuboid::from_length(0.90))),
+			|mesh| mesh.0.clone(),
+		);
+
+		for entity in entities {
+			commands.entity(entity).insert((
+				RigidBody::Dynamic,
+				Collider::cuboid(0.75, 0.75, 0.75),
+				Mesh3d(mesh.clone()),
 			));
 		}
 	}
